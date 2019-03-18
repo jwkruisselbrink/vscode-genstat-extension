78c23496bfb0879e7cd2b3da0a0c0f046afe3fb1
Johannes Kruisselbrink <johannes@kruisselbrink.eu>
2019-03-18 07:54:05 +0100

    Untabify documents

--- src/extension.ts
+++ src/extension.ts
@@ -15,98 +15,98 @@ const status: any = {};
 // the extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	const myScheme = 'genstatOutput';
-	const myOutputContentProvider = new OutputContentProvider();
+    const myScheme = 'genstatOutput';
+    const myOutputContentProvider = new OutputContentProvider();
 
-	context.subscriptions.push(
-		vscode.workspace.registerTextDocumentContentProvider(myScheme, myOutputContentProvider)
-	);
+    context.subscriptions.push(
+        vscode.workspace.registerTextDocumentContentProvider(myScheme, myOutputContentProvider)
+    );
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand('genstat.runGenStat', () => runGenStat(myOutputContentProvider))
-	);
+    context.subscriptions.push(
+        vscode.commands.registerCommand('genstat.runGenStat', () => runGenStat(myOutputContentProvider))
+    );
 
-	context.subscriptions.push(
-		vscode.commands.registerTextEditorCommand('genstat.openHelp', async () => {
-			openGenStatHelp();
-		})
-	);
+    context.subscriptions.push(
+        vscode.commands.registerTextEditorCommand('genstat.openHelp', async () => {
+            openGenStatHelp();
+        })
+    );
 }
 
 // method called when the extension is deactivated
 export function deactivate() { }
 
 async function openGenStatHelp(): Promise<void> {
-	const activeTextEditor = vscode.window.activeTextEditor;
-	let position = activeTextEditor.selection.start;
-	let wordRange = activeTextEditor.document.getWordRangeAtPosition(position);
-	let word = wordRange ? activeTextEditor.document.getText(wordRange) : '';
-	let lineText = activeTextEditor.document.lineAt(position.line).text;
-	let lineTillCurrentPosition = lineText.substr(0, wordRange.start.character);
+    const activeTextEditor = vscode.window.activeTextEditor;
+    let position = activeTextEditor.selection.start;
+    let wordRange = activeTextEditor.document.getWordRangeAtPosition(position);
+    let word = wordRange ? activeTextEditor.document.getText(wordRange) : '';
+    let lineText = activeTextEditor.document.lineAt(position.line).text;
+    let lineTillCurrentPosition = lineText.substr(0, wordRange.start.character);
 
-	let topic = "";
-	let wordPart = word.substr(0,4);
-	if (/^\s*$/.test(lineTillCurrentPosition)) {
-		var keywords = genstatKeywords.filter(r => r.substr(0,4) === wordPart);
-		if (keywords.length > 0) {
-			topic = keywords[0].substr(0, 8);
-		}
-	}
+    let topic = "";
+    let wordPart = word.substr(0,4);
+    if (/^\s*$/.test(lineTillCurrentPosition)) {
+        var keywords = genstatKeywords.filter(r => r.substr(0,4) === wordPart);
+        if (keywords.length > 0) {
+            topic = keywords[0].substr(0, 8);
+        }
+    }
 
-	const pathGenHelp = `C:/Program Files/Gen19Ed/Doc/Genstat.chm`;
-	let cmd = "";
-	if (!topic) {
-		cmd = `hh.exe ${pathGenHelp}`;
-	} else {
-		cmd = `hh.exe ${pathGenHelp}::/html/server/${topic}.htm`;
-	}
-	return cp.execSync(cmd);
+    const pathGenHelp = `C:/Program Files/Gen19Ed/Doc/Genstat.chm`;
+    let cmd = "";
+    if (!topic) {
+        cmd = `hh.exe ${pathGenHelp}`;
+    } else {
+        cmd = `hh.exe ${pathGenHelp}::/html/server/${topic}.htm`;
+    }
+    return cp.execSync(cmd);
 }
 
 async function runGenStat(provider: OutputContentProvider): Promise<void> {
-	if (!status.compile) {
-		status.compile = "run";
+    if (!status.compile) {
+        status.compile = "run";
 
-		const pathGenBatch = VscodeSettings.getInstance().pathGenBatch;
-		if (!pathGenBatch) {
-			vscode.window.showInformationMessage(`Error: GenBatch path not specified!`);
-			delete status.compile;
-			return;
-		}
+        const pathGenBatch = VscodeSettings.getInstance().pathGenBatch;
+        if (!pathGenBatch) {
+            vscode.window.showInformationMessage(`Error: GenBatch path not specified!`);
+            delete status.compile;
+            return;
+        }
 
-		const activeTextEditor = vscode.window.activeTextEditor;
-		if (!activeTextEditor) {
-			vscode.window.showInformationMessage(`Error: no GenStat file in active editor window!`);
-			delete status.compile;
-			return;
-		}
+        const activeTextEditor = vscode.window.activeTextEditor;
+        if (!activeTextEditor) {
+            vscode.window.showInformationMessage(`Error: no GenStat file in active editor window!`);
+            delete status.compile;
+            return;
+        }
 
-		const wad = activeTextEditor.document;
-		const filePath = wad.fileName;
-		GenStatOutputChannel.start(`Running GenStat file ${filePath}`);
+        const wad = activeTextEditor.document;
+        const filePath = wad.fileName;
+        GenStatOutputChannel.start(`Running GenStat file ${filePath}`);
 
-		await wad.save();
-		const basename = path.basename(filePath, path.extname(filePath));
-		const outPath = path.join(path.dirname(filePath), basename + ".lis");
-		const cmd = `"${pathGenBatch}" IN="${filePath}" "${outPath}"`;
-		try {
-			await vscode.window.withProgress({
-				location: vscode.ProgressLocation.Window,
-				title: "Running GenStat...",
-			}, async () => {
-				await execPromise(cmd);
-				GenStatOutputChannel.end(`Run GenStat complete!`);
-			});
-		} catch (ex) {
-			GenStatOutputChannel.error(`Run GenStat failed: ${ex.message}!`);
-		} finally {
-			if (fs.existsSync(outPath)) {
-				await showGenStatOutput(outPath, provider);
-			}
-		}
+        await wad.save();
+        const basename = path.basename(filePath, path.extname(filePath));
+        const outPath = path.join(path.dirname(filePath), basename + ".lis");
+        const cmd = `"${pathGenBatch}" IN="${filePath}" "${outPath}"`;
+        try {
+            await vscode.window.withProgress({
+                location: vscode.ProgressLocation.Window,
+                title: "Running GenStat...",
+            }, async () => {
+                await execPromise(cmd);
+                GenStatOutputChannel.end(`Run GenStat complete!`);
+            });
+        } catch (ex) {
+            GenStatOutputChannel.error(`Run GenStat failed: ${ex.message}!`);
+        } finally {
+            if (fs.existsSync(outPath)) {
+                await showGenStatOutput(outPath, provider);
+            }
+        }
 
-		delete status.compile;
-	}
+        delete status.compile;
+    }
 }
 
 function execPromise(command) {
@@ -122,9 +122,9 @@ function execPromise(command) {
 }
 
 async function showGenStatOutput(fileName: string, provider: OutputContentProvider): Promise<void> {
-	const basename = path.basename(fileName);
-	let uri = vscode.Uri.parse(`genstatOutput:${basename}`);
-	let doc = await vscode.workspace.openTextDocument(uri);
-	await vscode.window.showTextDocument(doc, { preview: false, viewColumn: ViewColumn.Beside, preserveFocus: true });
-	provider.onDidChangeEmitter.fire(uri);
+    const basename = path.basename(fileName);
+    let uri = vscode.Uri.parse(`genstatOutput:${basename}`);
+    let doc = await vscode.workspace.openTextDocument(uri);
+    await vscode.window.showTextDocument(doc, { preview: false, viewColumn: ViewColumn.Beside, preserveFocus: true });
+    provider.onDidChangeEmitter.fire(uri);
 }
--- src/utils.ts
+++ src/utils.ts
@@ -2,26 +2,26 @@ import { TextDocument, Position } from "vscode";
 import { genstatKeywords } from "./genstatKeywords";
 
 export function adjustWordPosition(document: TextDocument, position: Position): [boolean, string, Position] {
-	let wordRange = document.getWordRangeAtPosition(position);
-	let lineText = document.lineAt(position.line).text;
-	let word = wordRange ? document.getText(wordRange) : '';
-	if (!wordRange || lineText.startsWith('//') || isPositionInString(document, position) || word.match(/^\d+.?\d+$/) || genstatKeywords.indexOf(word) > 0) {
-	 	return [false, null, null];
-	}
-	if (position.isEqual(wordRange.end) && position.isAfter(wordRange.start)) {
- 	    position = position.translate(0, -1);
+    let wordRange = document.getWordRangeAtPosition(position);
+    let lineText = document.lineAt(position.line).text;
+    let word = wordRange ? document.getText(wordRange) : '';
+    if (!wordRange || lineText.startsWith('//') || isPositionInString(document, position) || word.match(/^\d+.?\d+$/) || genstatKeywords.indexOf(word) > 0) {
+         return [false, null, null];
     }
-	return [true, word, position];
+    if (position.isEqual(wordRange.end) && position.isAfter(wordRange.start)) {
+         position = position.translate(0, -1);
+    }
+    return [true, word, position];
 }
 
 export function isPositionInString(document: TextDocument, position: Position): boolean {
-	let lineText = document.lineAt(position.line).text;
-	let lineTillCurrentPosition = lineText.substr(0, position.character);
+    let lineText = document.lineAt(position.line).text;
+    let lineTillCurrentPosition = lineText.substr(0, position.character);
 
-	// Count the number of double quotes in the line till current position. Ignore escaped double quotes
-	let doubleQuotesCnt = (lineTillCurrentPosition.match(/\"/g) || []).length;
-	let escapedDoubleQuotesCnt = (lineTillCurrentPosition.match(/\\\"/g) || []).length;
+    // Count the number of double quotes in the line till current position. Ignore escaped double quotes
+    let doubleQuotesCnt = (lineTillCurrentPosition.match(/\"/g) || []).length;
+    let escapedDoubleQuotesCnt = (lineTillCurrentPosition.match(/\\\"/g) || []).length;
 
-	doubleQuotesCnt -= escapedDoubleQuotesCnt;
-	return doubleQuotesCnt % 2 === 1;
+    doubleQuotesCnt -= escapedDoubleQuotesCnt;
+    return doubleQuotesCnt % 2 === 1;
 }
